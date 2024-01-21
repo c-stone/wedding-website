5d356de01a79f6a9ea556beb6d449ce4105a96e4
Chris Stone <chris.stone603@gmail.com>
2024-01-21 18:03:53 -0500

    adds visual error message for name input

--- project-folder/js-package/components/NameEntryForm.jsx
+++ project-folder/js-package/components/NameEntryForm.jsx
@@ -2,22 +2,25 @@ import React, { useState } from 'react';
 
 function NameEntryForm({ setGuestDetails, guests }) {
   const [guestName, setGuestName] = useState('');
+  const [errorMessage, setErrorMessage] = useState(''); // New state variable for error message
+
   const handleNameChange = (event) => {
     setGuestName(event.target.value);
+    setErrorMessage(''); // Reset error message on name change
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const guest = guests.find(
-      (g) => g.name.toLowerCase() === guestName.toLowerCase(),
+      (g) => g.name.toLowerCase() === guestName.toLowerCase().trim(),
     );
-    console.log(guest);
+
     if (guest) {
       setGuestDetails(guest);
+      setErrorMessage(''); // Reset error message on successful find
     } else {
-      // Handle no guest found
       setGuestDetails(null);
-      console.log('Guest not found. Please try again.');
+      setErrorMessage(`"${guestName.trim()}" not found. Please try again.`); // Set error message
     }
   };
 
@@ -32,6 +35,9 @@ function NameEntryForm({ setGuestDetails, guests }) {
           onChange={handleNameChange}
         />
       </label>
+      {errorMessage && (
+        <div className="text-red-500">{errorMessage}</div> // Display error message if it exists
+      )}
       <button
         className="bg-amber-700 hover:bg-amber-900 text-white py-4 px-8 rounded tracking-widest max-w-72"
         type="submit"
